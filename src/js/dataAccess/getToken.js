15e9a04a15e32716493d5e73c094ff4bf047161d
Michael.Samula <msamula@web.de>
2022-10-27 13:23:39 +0200

    0.4.4
    getImage with fetch
    getTemp with fetch
    refreshToken with fetch
    
    beide funktionen in seperate intervalle

--- src/js/dataAccess/getToken.js
+++ src/js/dataAccess/getToken.js
@@ -24,21 +24,21 @@ export function getToken(ip,cliId,cliSecret,user,password)
 }
 
 //Refresh token function
-export function refreshToken(ip,cliId,cliSecret,refreshtoken){
+export async function refreshToken(ip,cliId,cliSecret,refreshToken){
     let token;
-    let url = `http://${ip}/api/oauth/token?client_id=${cliId}&client_secret=${cliSecret}&grant_type=refresh_token&refresh_token=${refreshtoken}`;
+    let url = `http://${ip}/api/oauth/token?client_id=${cliId}&client_secret=${cliSecret}&grant_type=refresh_token&refresh_token=${refreshToken}`;
 
-    xmlHttp.open('POST', url, false);
-    xmlHttp.setRequestHeader('accept', 'application/json');
-
-    xmlHttp.onreadystatechange = function() {
-        if(xmlHttp.readyState === 4 && xmlHttp.status === 200) {
-
-            let response = JSON.parse(xmlHttp.responseText);
-            token = new Token(response['access_token'],response['token_type'],response['expires_in'],response['scope'],response['refresh_token'],response['iat'],response['exp']);
+    let response =await fetch(url,{
+        method: 'POST',
+        headers: {
+        'accept': 'application/json',
+            'Authorization': `Bearer ${token}`
         }
-    }
+    });
 
-    xmlHttp.send();
+    let json = await response.json();
+
+    token = await new Token(json['access_token'],json['token_type'],json['expires_in'],json['scope'],json['refresh_token'],json['iat'],json['exp']);
     return token;
+
 }
