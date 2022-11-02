47205032d3ae0194f05e48f809d53d6304a46c26
Michael.Samula <msamula@web.de>
2022-11-02 14:59:05 +0100

    0.4.8.2 checkToken

--- src/js/app.js
+++ src/js/app.js
@@ -2,12 +2,13 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/custom.css';
 
-import {getToken, refreshToken} from "./dataAccess/getToken";
+import {addEventListener} from "./userInterface/eventListener";
+import {getToken} from "./dataAccess/getToken";
+import {getJobInfo} from "./dataAccess/getJobInfo";
+import {drawAOI} from "./userInterface/drawAOI";
 import {getImage} from "./dataAccess/getImage";
 import {getData} from "./dataAccess/getTemp";
-import {getJobInfo} from "./dataAccess/getJobInfo";
-import {addEventListener} from "./userInterface/eventListener";
-import {drawAOI} from "./userInterface/drawAOI";
+
 
 //login data
 let ipAddress = 'localhost:8080';
@@ -16,29 +17,24 @@ let clientSecret = 'MnrY2L86pEQr53!6';
 let user = 'administrator';
 let password = 'administrator';
 
-//MAIN PART
-try {
-    //load function for button
-    addEventListener();
+//--- MAIN PART ---
+
+//load function for button
+addEventListener();
 
 
-    //get token
-    getToken(ipAddress, clientID, clientSecret, user, password);
+//get token
+getToken(ipAddress, clientID, clientSecret, user, password);
 
 
-    //get thresholds and coordinates for aoi from the COFFEE CUP JOB!
-    let jobInfo = getJobInfo(ipAddress,'Coffeecup');
+//get thresholds and coordinates for aoi from the COFFEE CUP JOB!
+let jobInfo = getJobInfo(ipAddress,'Coffeecup');
 
 
-    //draw AOI's / jobInfo[1] => coordinates of aoi
-    drawAOI(jobInfo[1]);
+//draw AOI's / jobInfo[1] => coordinates of aoi
+drawAOI(jobInfo[1]);
 
 
-    //get image and data from camera
-    getImage(ipAddress);
-    getData(ipAddress, jobInfo[0][2]);
-}
-catch(err)
-{
-    console.log(err);
-}
+//get image and data from camera
+getImage(ipAddress);
+getData(ipAddress, jobInfo[0][2]);
