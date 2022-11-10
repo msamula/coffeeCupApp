98de11d7a345e94a1031f7a64e34c66022d18344
Michael.Samula <msamula@web.de>
2022-11-10 12:35:05 +0100

    0.5.6
    URL.revokeObjectURL
    --> memory probleme behoben

--- src/js/dataAccess/getImage.js
+++ src/js/dataAccess/getImage.js
@@ -18,6 +18,9 @@ export async function getImage(ip)
     if (response.status === 200) {
         let blob = await response.blob();
         image.src = URL.createObjectURL(blob);
+
+        // The URL.revokeObjectURL() static method releases an existing object URL which was previously created by calling URL.createObjectURL().
+        // Call this method when you've finished using an object URL to let the browser know not to keep the reference to the file any longer.
         image.onload = () => {
             URL.revokeObjectURL(image.src);
         }
